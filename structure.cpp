09fd416d8eac71c1acd6fe3c25c72359ebf161bf
Neil Angelito La Valle Manuel <neilalamok@gmail.com>
2016-12-17 16:58:34 +0800

    Update structure.cpp

--- structure.cpp
+++ structure.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
 using namespace std;
 
+
 struct Person
 {
 	int toes;
 	int knees;
 	int shoulder;
 	int head;
-}
+};
+ int main()
+ {
+	 Person person;
+	 cout << "Person's toes:" << endl;
+	 cin >> person.toes;
+	 cout << "Person's knees:" << endl;
+	 cin>> person.knees;
+	 cout<< shoulder:" << endl;
+	 cin>> person.shoulder;
+	 cout<< head:" << endl;
+	 cin>> person.head;
+	 
+	 cout << person.toes << endl;
+	 cout << person.knees << endl;
+	 cout << person.shoulder << endl;
+	 cout << person.head << endl;
+	 return 0;
+ }
